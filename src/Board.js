ab7794c10ae6cd9d01207da5b1fd3e7cb9d639f5
unknown <rpmulrennan@gmail.com>
2021-09-27 11:50:47 -0500

    added logger for board plays

--- src/Board.js
+++ src/Board.js
@@ -14,61 +14,32 @@ function Board() {
     }
   
     //updates turn and sets value of the mark to pass to the square component
-    const handleTurn = () => {
-         
+    const handleTurn = (i) => {
         (playerTurn) ? setMark("X") : setMark("O");
         updateTurn();
+        var tempSquareVals = Array(9); 
+        tempSquareVals = squareVals.slice(); //copy the current board array
+        tempSquareVals[i] = mark; //update the array for new mark made
+        setsquareVals(tempSquareVals); //replace the current board array
+        console.log(tempSquareVals); 
     }
-
-
-
 
         return(
             <div className="board"> 
                 <div className="board__row"> 
-                    <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-                    <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-                                        <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-  
+                <Square turn={mark} handleTurn = {handleTurn} id = {0}/>
+                <Square turn={mark} handleTurn = {handleTurn} id = {1}/>
+                <Square turn={mark} handleTurn = {handleTurn} id = {2}/>
                 </div> 
                 <div className="board__row"> 
-                    <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-                    <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-                                        <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-
+                    <Square turn={mark} handleTurn = {handleTurn} id = {3}/>
+                    <Square turn={mark} handleTurn = {handleTurn} id = {4}/>
+                    <Square turn={mark} handleTurn = {handleTurn} id = {5}/> 
                 </div> 
                 <div className="board__row"> 
-                    <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-                    <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-                                        <Square 
-                        turn={mark} 
-                        handleTurn = {handleTurn} 
-                    />
-
+                    <Square turn={mark} handleTurn = {handleTurn} id = {6}/>
+                    <Square turn={mark} handleTurn = {handleTurn} id = {7}/>
+                    <Square turn={mark} handleTurn = {handleTurn} id = {8}/>
                 </div> 
 
                 
--- src/Square.js
+++ src/Square.js
@@ -7,27 +7,13 @@ function Square(props) {
     const [mark, setMark] = useState("");
 
     const updateMark = () => {
-        props.handleTurn();
+        props.handleTurn(props.id);
         setMark(props.turn); 
     }
    
     return (
-        <botton className="square__button" 
-        //onClick={props.handleTurn}
-        //onChange = {handleMark}
-        onClick = {updateMark} 
-        
-      //  onClick = {setMark(props.turn)}//{props.handleTurn}
-        >{mark}
-        
-        </botton>
+        <botton className="square__button" onClick = {updateMark}> {mark} </botton>
     )
-  
- 
-
-
-
-
 }
 
 export default Square
